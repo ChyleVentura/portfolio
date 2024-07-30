30c614be62463103854dd0664671043a26a4650c
ChyleVentura <kyleventura424@gmail.com>
2024-07-30 11:54:16 +0800

    changes

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
     <div className='text-white z-0 lg:px-40 px-5'>
         <div id="hero-title"className='flex-col flex max-w-[1000] h-screen w-full justify-center items-start mx-auto py-[10rem]'>
             <p className='text-bold text-2xl text-violet-500'>Chyle Ventura</p>
-            <h1 className='lg:text-[160px] md:text-[130px] text-wrap text-7xl
+            <h1 className='lg:text-[160px] md:text-[120px] text-wrap text-5xl
             tracking-tighter text-bold leading-[.75em]'>
               FRONT-END DEVELOPER VIDEO EDITOR</h1>
         </div>
