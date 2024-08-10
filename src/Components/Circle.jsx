c5c2063923a64aa71e527e6a2fa57ead468b419e
ChyleVentura <kyleventura424@gmail.com>
2024-08-10 14:33:18 +0800

    changes

--- src/Components/Circle.jsx
+++ src/Components/Circle.jsx
@@ -11,7 +11,7 @@ const Circle = () => {
     <>
     <div className="circle_wrapper fixed justify-center w-full items-center flex h-screen -z-40 overflow-hidden">
              
-        <div id="circle" className=" bg-secondary/50 w-[600px] h-[600px] rounded-[50%] blur-[300px]">
+        <div id="circle" className=" bg-secondary/50 md:w-[600px] md:h-[600px] w-[200px] h-[200px] rounded-[50%] blur-[50px] md:blur-[200px]">
         </div>      
     </div>
     <div id="target"className=''></div>
